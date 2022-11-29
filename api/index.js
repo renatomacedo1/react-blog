fec7030e5432ef9237f00371a0e6c694276e01d2
Renato Macedo <renato_macedo_1@hotmail.com>
2022-11-29 16:23:56 +0000

    categories and image upload

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -22,9 +24,27 @@ mongoose
     console.log(err);
   });
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    //cb -> callback
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    //cb(null, req.body.name);
+    cb(null, "image.jpeg");
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
