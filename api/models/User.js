82556ad1503dbbc711083efb6fd338cd76f1b123
Renato Macedo <renato_macedo_1@hotmail.com>
2022-11-30 16:34:22 +0000

    register page fetchs

--- api/models/User.js
+++ api/models/User.js
@@ -14,7 +14,8 @@ const UserSchema = new mongoose.Schema(
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "Please provide password"],
+      minlength: 6,
     },
     profilePicture: {
       type: String,
