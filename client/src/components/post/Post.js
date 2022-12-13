f02e65b2cd7ccad5a725d95667447070935f5e60
Renato Macedo <renato_macedo_1@hotmail.com>
2022-12-13 16:28:36 +0000

    fixed not returning categories

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,5 +1,5 @@
 import "./post.css";
-import img1 from "../../assets/postimg.jpeg";
+//import img1 from "../../assets/postimg.jpeg";
 import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
@@ -9,17 +9,19 @@ export default function Post({ post }) {
       {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
 
       <div className="postInfo">
-        <div className="postCats">
-          {post.categories.map((c, index) => {
-            <span className="postCat" key={index}>
-              {c.name}
-            </span>;
-          })}
-        </div>
         <Link to={`/post/${post._id}`} className="link">
           <span className="postTitle">{post.title}</span>
         </Link>
         <hr />
+        <div className="postCats">
+          {post.categories.map((c, index) => {
+            return (
+              <span className="postCat" key={index}>
+                {c.name}
+              </span>
+            );
+          })}
+        </div>
 
         <span className="postDate">
           {new Date(post.createdAt).toDateString()}
